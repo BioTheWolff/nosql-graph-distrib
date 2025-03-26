6f585085736d6f71e93e6bf1deaa34acac80464c
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2025-03-26 10:16:09 +0100

    feat(spark): move everything into activity 1 because it was
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- spark/Spark/src/main/scala/Activity1.scala
+++ spark/Spark/src/main/scala/Activity1.scala
@@ -2,9 +2,10 @@ package fr.umontpellier.polytech
 
 /* SimpleApp.scala */
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.collect_list
 
 object Activity1 {
-  def main(args: Array[String]): Unit = {
+  private def one(): Unit = {
     val logFile = "./data/spark.txt" // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").master("local[*]").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
@@ -12,5 +13,38 @@ object Activity1 {
     val numBs = logData.filter(line => line.contains("b")).count()
     println(s"Lines with a: $numAs, Lines with b: $numBs")
     spark.stop()
+  }
+
+  private def two(): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("UsersTest")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+    val usersDF = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("data/users.csv")
+
+    val result = usersDF
+      .filter($"age" >= 25)
+      .groupBy("city")
+      .agg(collect_list("name").as("names"))
+      .select("city", "names")
+
+    val resultCollected = result.collect()
+    resultCollected.foreach { row =>
+      val city = row.getAs[String]("city")
+      val names = row.getAs[Seq[String]]("names")
+      println(s"Users in $city: ${names.mkString(", ")}")
+    }
+    spark.stop()
+  }
+
+  def main(args: Array[String]): Unit = {
+    one()
+    two()
   }
 }
