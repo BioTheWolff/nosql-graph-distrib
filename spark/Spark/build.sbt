786de101485dfbc1977064a516794dc03b2ce6fd
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2025-03-26 11:02:36 +0100

    feat(spark): working scala code for activity 2
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- spark/Spark/build.sbt
+++ spark/Spark/build.sbt
@@ -1,11 +1,20 @@
-ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / version := "1.0.0"
 
 ThisBuild / scalaVersion := "2.12.18"
 
 ThisBuild / libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.5"
+ThisBuild / libraryDependencies += "org.apache.spark" %% "spark-hadoop-cloud" % "3.5.3"
+ThisBuild / libraryDependencies += "io.minio" % "spark-select_2.11" % "2.1"
 
 lazy val root = (project in file("."))
   .settings(
     name := "Spark",
-    idePackagePrefix := Some("fr.umontpellier.polytech")
+    idePackagePrefix := Some("fr.umontpellier.polytech"),
+    assembly / mainClass := Some("fr.umontpellier.polytech.Activity2"),
   )
+
+ThisBuild / assemblyMergeStrategy := {
+  case x if x.startsWith("META-INF/services/") => MergeStrategy.concat
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.preferProject
+}
--- spark/Spark/src/main/scala/Activity2.scala
+++ spark/Spark/src/main/scala/Activity2.scala
@@ -1,8 +1,62 @@
 package fr.umontpellier.polytech
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
+import com.amazonaws.SDKGlobalConfiguration
+import org.apache.spark.sql.functions.collect_list
 
 object Activity2 {
+  private def helper(endpoint: String): SparkSession = {
+    System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "true")
 
+    val user = "minio"
+    val password = "minio123"
+
+    SparkSession.builder()
+      .appName("MinIOActivity2")
+      .master("local[*]")
+      .config("spark.hadoop.fs.s3a.endpoint", endpoint)
+      .config("spark.hadoop.fs.s3a.access.key", user)
+      .config("spark.hadoop.fs.s3a.secret.key", password)
+      .config("spark.hadoop.fs.s3a.path.style.access", "true")
+      .config("spark.hadoop.fs.s3a.connection.timeout", "100000")
+      .getOrCreate()
+  }
+
+  private def processCSV(spark: SparkSession): Unit = {
+
+    val path = "s3a://mybucket/users.csv"
+
+    import spark.implicits._
+    val users = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(path)
+
+    val result = users
+      .filter($"age" >= 25)
+      .groupBy("city")
+      .agg(collect_list("name").as("names"))
+      .select("city", "names")
+
+    val resultCollected = result.collect()
+    resultCollected.foreach { row =>
+      val city = row.getAs[String]("city")
+      val names = row.getAs[Seq[String]]("names")
+      println(s"Users in $city: ${names.mkString(", ")}")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    var endpoint = "https://minio.s3.svc.cluster.local:9000/"
+    if (args.length > 0)
+      endpoint = args(0)
+
+    println(s"Using endpoint: $endpoint")
+
+    val spark = helper(endpoint)
+
+    processCSV(spark)
+
+    spark.stop()
+  }
 }
